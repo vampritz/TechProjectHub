ec0d2fa6135434ed80433a39cbda77bd21b66ad9
Ritesh Srivatava <43913821+vampritz@users.noreply.github.com>
2021-08-09 11:54:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TechProjectHub
+# ProjectTech.github.io
+
+A website where You can get Many types of projects
+[https://techprojecthub.netlify.app/](https://techprojecthub.netlify.app/)
---
+++ javascript/JS.js
@@ -0,0 +1,160 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Data Science", "Web Development",
+                     "Artificial Inteligence","Cyber Security", "Python","Java","C++"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+    if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
+var prelaoder=document.getElementById("loading")
+  function myfunction(){
+    prelaoder.style.display='none';
+  }
+
+
+
+
+
+
+
+function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+      document.querySelector('.status').innerHTML = "Name cannot be empty";
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.querySelector('.status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      return false;
+  }
+  document.querySelector('.status').innerHTML = "Sending...";
+}
+
+
+
+function AlertIt() {
+var answer = confirm ("Comming soon")
+
+}
+
+
+
+
+
+
+
+// top function
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+let a;
+let t=document.getElementById('time');
+let d=document.getElementById('date');
+setInterval(() => {
+
+    a=new Date();
+    ho= ("0"+a.getHours()).slice(-2)
+
+    b=parseInt(ho);
+    if (b>12){
+      g="PM"
+
+    }
+    else{
+      g="AM"
+    }
+
+    b=("0"+(b%12).toString()).slice(-2)
+
+
+    mi=("0"+a.getMinutes()).slice(-2)
+    sec=("0"+a.getSeconds()).slice(-2)
+
+
+    t.innerHTML=b+':'+ mi + ':'+ sec +"  "+g
+
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(a);
+    let mo=a.toLocaleString('default',{month: 'short'})
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(a);
+    d.innerHTML=da+'/'+mo+'/'+ye
+    }, 1000);
